1ee37c9b06373149f3947a52d31274f180d45bea
valdljr <valdir.lago@bling.com.br>
2020-11-25 18:31:27 -0300

    test: add tests

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -86,9 +86,5 @@ export const AuthProvider: React.FC = ({ children }) => {
 export function useAuth(): AuthContextState {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-
   return context;
 }
